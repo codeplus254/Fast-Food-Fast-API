0cb9a9228cc6a19b02a0e99c66607b472ef90076
Ronny Mageh <ronnymageh@gmail.com>
2018-09-08 00:00:32 +0300

    Added the tests again

--- get_list_of_all_orders.py
+++ get_list_of_all_orders.py
@@ -198,4 +198,14 @@ def show_order(orderId):
         return "Put request filed"
         
 
+def test_app(client):
+    response = client.get('/api/v1/orders')
+    assert response.status_code == 200
 
+def test_specific_order(client):
+    response = client.get('/api/v1/orders/1')
+    assert response.status_code == 200
+
+def test_invalid_order_id(client):
+    response = client.get('api/v1/orders/300')
+    assert b'Invalid! Please check your order id' in response.data
