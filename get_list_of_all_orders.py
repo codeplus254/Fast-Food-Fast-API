52daae7e329d78c3661ead01d792123e6ed996d9
Ronny Mageh <ronnymageh@gmail.com>
2018-09-07 17:50:26 +0300

    User can now see orders placed

--- get_list_of_all_orders.py
+++ get_list_of_all_orders.py
@@ -1,7 +1,7 @@
 """This python file is responsible for showing all user orders"""
 
 from flask import Flask
-#from flask import request
+from flask import request
 
 
 APP = Flask(__name__)
@@ -16,16 +16,43 @@ def index():
 @APP.route('/api/v1/orders', methods=['GET', 'POST'])
 def orders():
     """when the user of the api goes to <url>/orders s/he gets an input form"""
-    if request.method == "POST":
-            
-        return '''<form method="POST">
-                      Food: <input type="text" name="language"><br>
-                      Quantity: <input type="text" name="framework"><br>
+    """Place an order for food"""
+    '''<html>
+                <form method="POST" action="http://localhost:5000/api/v1/orders">
+                      Food: <input type="text" name="food"><br>
+                      Quantity: <input type="text" name="quantity"><br>
                       <input type="submit" value="Submit"><br>
-                  </form>'''
-    e
-
-def test_orders(client,app):
-     assertEqual( client.get('/api/v1/orders').status_code, 400)
+                  </form>
+            </html>''' 
     
+    # treat POST request 
+    # treat POST request 
     
+    if request.method == 'POST':
+        food = request.form['food']
+        return "You've placed an order for "+food
+    else:
+        
+        return '''<html>
+                <form method="POST" action="http://localhost:5000/api/v1/orders">
+                      Food: <input type="text" name="food"><br>
+                      Quantity: <input type="text" name="quantity"><br>
+                      <input type="submit" value="Submit"><br>
+                  </form>
+            </html>'''
+#def test_orders(client,app):
+     #assertEqual( client.get('/api/v1/orders').status_code, 400)
+    
+@APP.route('/api/v1/orders/<orderId>', methods=['GET', 'PUT'])
+def order():
+    """when the user of the api goes to <url>/orders s/he gets an input form"""
+    """Place an order for food"""
+    '''<html>
+                <form method="POST" action="http://localhost:5000/api/v1/orders">
+                      Food: <input type="text" name="food"><br>
+                      Quantity: <input type="text" name="quantity"><br>
+                      <input type="submit" value="Submit"><br>
+                  </form>
+            </html>''' 
+    if request.method == 'GET':
+        return "ECHO: GET\n"
