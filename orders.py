3d77a91d132cb45c129e9ebca0cd5e27a33a3cf2
Ronny Mageh <ronnymageh@gmail.com>
2018-09-09 11:51:51 +0300

    Fluid version of the pap. All major bugs fixed!

--- orders.py
+++ orders.py
@@ -1,201 +1,94 @@
 """This python file is responsible for showing all user orders"""
 
-from flask import Flask
-from flask import request,send_file
+from flask import Flask,request,send_file
+
 
 
 APP = Flask(__name__)
-
+ 
 
 @APP.route('/')
 def index():
     """This function shows the home page of the api"""
     return 'Fast Food Fast API'
-
+@APP.route('/api/v1/images/<imageId>')
+def get_image(imageId):
+    """This function gets food icons"""
+    return send_file('images/'+imageId, mimetype='image/jpeg')
 
 
 @APP.route('/api/v1/orders', methods=['GET', 'POST'])
 def orders():
-    """when the user of the api goes to <url>/orders s/he gets an input form"""
-    """Place an order for food"""
-    '''<html>
-                <form method="POST" action="https://fast-food-fast-api-v1.herokuapp.com/api/v1/orders">
-                    <h3> Place your order for food </h3>
-                    <select name= "order1">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order2">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order3">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order4">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order5">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                      <input type="submit" value="Submit"><br>
-                  </form>
-            </html>''' 
-    
+    """when the user of the api goes to <url>/api/v1/orders s/he gets the menu and selects. On submission, the user gets to see the selected items"""
+ 
     # treat POST request 
     # treat POST request 
     
     if request.method == 'POST':
-        global order1
-        order1 = request.form.get('order1')
-        global order2
-        order2 = request.form.get('order2')
-        global order3
-        order3 = request.form.get('order3')
-        global order4
-        order4 = request.form.get('order4')
-        global order5
-        order5 = request.form.get('order5')
-
-        return "You've ordered for: "+order1 + ", "+order2 + ", "+order3 + ", "+order4 + ", "+order5 + "."
-    else:
+     
+        #append the orders to a list if they've been selected
+        global my_orders
+        my_orders = request.form.getlist('food')
+        
+        return "You've order for: "+', '.join(my_orders)     #prints out the list values on html
+    elif request.method == 'GET':
         
         return '''<html>
-                <form method="POST" action="https://fast-food-fast-api-v1.herokuapp.com/api/v1/orders">
-                    <h3> Place your order for food </h3>
-                    <select name= "order1">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order2">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order3">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order4">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <select name= "order5">
-                        <option value=" " > </option>
-                        <option value="Burger and Hamketchup">Burger and Hamketchup</option>
-                        <option value="Five Bake Donuts">Five Bake Donuts</option>
-                        <option value="Fresh Salad">Fresh Salad</option>
-                        <option value="Fried Meat">Fried Meat</option>
-                        <option value="Fries and Burger">Fries and Burger</option>
-                    </select><br>
-                    <input type="submit" value="Submit"><br>
-                  </form>
-            </html>'''
+                    <form method="POST" >
+                        <h3> Place your order for food </h3>
+                        <input type="checkbox" name="food" value="Burger and Hamketchup" ><input type="image" src="images/burger_and_hamketchup.jpeg" alt="Burger and Hamketchup" width="100px" height="50px"> Burger and Hamketchup<br>
+                            <input type="checkbox" name="food" value="Five Bake Donuts" ><input type="image" src="images/five_bake_donuts.jpeg" alt="Five Bake Donuts" width="100px" height="50px" > Five Bake Donuts<br>
+                            <input type="checkbox" name="food" value="Fresh Salad" ><input type="image" src="images/fresh_salad.jpeg" alt="Fresh Salad" width="100px" height="50px" >Fresh Salad<br>
+                            <input type="checkbox" name="food" value="Fried Meat" ><input type="image" src="images/fried_meat.jpeg" alt="Fried Meat" width="100px" height="50px">Fried Meat<br>
+                            <input type="checkbox" name="food" value="Fries and Burger" ><input type="image" src="images/fries_and_burger.jpeg" alt="Fries and Burger" width="100px" height="50px">Fries and Burger<br>
+                            <input type="checkbox" name="food" value="Hamburger" ><input type="image" src="images/hamburger.jpeg" alt="Hamburger" width="100px" height="50px">Hamburger<br>
+                            <input type="checkbox" name="food" value="Healthy Burger" ><input type="image" src="images/healthy_burger.jpeg" alt="Healthy Burger" width="100px" height="50px">Healthy Burger<br>
+                            <input type="checkbox" name="food" value="Meat Burger" ><input type="image" src="images/meat_burger.jpg" alt="Meat Burger" width="100px" height="50px">Meat Burger<br>
+                            <input type="checkbox" name="food" value="Pizza with egg" ><input type="image" src="images/pizza_with_egg.jpeg" alt="Pizza with egg" width="100px" height="50px">Pizza with egg<br>
+                            <input type="checkbox" name="food" value="Popcorn" ><input type="image" src="images/popcorn.jpeg" alt="Popcorn" width="100px" height="50px">Popcorn<br>
+                            <input type="checkbox" name="food" value="Sliced Cheeseburger" ><input type="image" src="images/sliced_cheeseburger.jpeg" alt="Sliced Cheeseburger" width="100px" height="50px">Sliced Cheeseburger<br>
+                            <input type="checkbox" name="food" value="Pepperoni Pizza" ><input type="image" src="images/pepperoni_pizza.jpeg" alt="Pepperoni Pizza" width="100px" height="50px">Pepperoni Pizza<br>
+
+                        <input type="submit" value="Submit"><br>
+                    </form>
+                </html>''' 
+        
 #def test_orders(client,app):
      #assertEqual( client.get('/api/v1/orders').status_code, 400)
     
-@APP.route('/api/v1/orders/<int:orderId>', methods=['GET', 'PUT'])
+@APP.route('/api/v1/orders/<int:orderId>', methods=['GET', 'PUT','POST'])
 def show_order(orderId):
-    """when the user of the api goes to <url>/orders s/he gets an input form"""
-    """Place an order for food"""
-    '''<html>
-        <form method="POST" action="https://fast-food-fast-api-v1.herokuapp.com/api/v1/orders/<int:orderId>">
-            <input type="hidden" name="_METHOD" value="PUT"/>
-            <h3> Confirm / Reject Order </h3>
-                    <select name= "change">
-        
-                        
-                        <option value="Confirm">Confirm</option>
-                        <option value="Reject">Reject</option>
-                    </select><br>
-                    <input type="submit" value="Submit"><br>
-        </form>
-        </html>'''
-    if request.method == 'GET':
-       
-        if orderId==1:
-            try: 
+    """when the user of the api goes to <url>/api/v1/orders/<int:orderId> s/he gets the specific order if it exists"""
+    
+    if request.method == 'PUT' or request.method == 'POST':
+        my_status = request.form.getlist('status') 
+        if (str(my_status)=="['confirm']"):
+            return "Order ID: "+str(orderId)+ "  " + my_orders[orderId-1] + " Confirmed"
+        else:
+            del my_orders[orderId-1]
+            return "Order ID: "+str(orderId)+ "   rejected"
+    elif request.method == 'GET':
+        #first check if orderId is in list. OrderId will be the index of list +1 since list index start from zero
+        try:
+            if (orderId<=len(my_orders)):     #ensure orderId is within range
+                return '''<html>
+                            <form method="POST" >
+                                <h3> Please confirm or reject the order first </h3><br>
+                                <input type="radio" name="status" value="confirm" checked/>
+                                <label for="confirm">Confirm</label><br>
+                                <input type="radio" name="status" value="reject" />
+                                <label for="reject">Reject</label><br>
 
-                if order1 == " ":
-                    return "No order filled"
-                else: 
-                    return order1
-            except NameError:
-                return "Order not yet filled"
-        elif orderId==2:
-            try: 
-                if order2 == " ":
-                    return "No order filled"
-                else: 
-                    return order2
-            except NameError:
-                return "Order not yet filled"
-        elif orderId==3:
-            try:
-                if order3 == " ":
-                    return "No order filled"
-                else: 
-                    return order3
-            except NameError:
-                return "Order not yet filled"
-        elif orderId==4:
-            try:
-                if order2 == " ":
-                    return "No order filled"
-                else: 
-                    return order4
-            except NameError:
-                return "Order not yet filled"
-        elif orderId==5:
-            try:
-                if order2 == " ":
-                    return "No order filled"
-                else: 
-                    return order5
-            except NameError:
-                return "Order not yet filled"
+                                
+                                <input type="submit" value="Submit"><br>
+                            </form>
+                        </html> '''
+        #if the orderId is not in range 
+        except IndexError:
+            return "No such order exists!"
         else:
             return "Invalid! Please check your order id"
-    elif request.method == 'PUT':   
-        return "Put request filed"
+    
         
 def test_app(client):
     response = client.get('/api/v1/orders')
