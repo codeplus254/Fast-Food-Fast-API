03f1008b8b7e912ee6c03a8cdee60ce985bfa04b
Ronny Mageh <ronnymageh@gmail.com>
2018-09-26 09:12:33 +0300

    updated the api_v1.py file to reflect the current one in develop

--- api/v1/api_v1.py
+++ api/v1/api_v1.py
@@ -23,8 +23,8 @@ def orders():
         REQUESTED_DATA.append(request.get_json())     #one can make more than one post request
         global CURRENT_ID,ORDER_ID, ORDER_CONTACT, ORDER_DELIVERY_ADDRESS
         ORDER_ID, ORDER_CONTACT, ORDER_DELIVERY_ADDRESS = [], [], []
-        global ORDER_NAME, ORDER_PRICE, ORDER_QUANTITY
-        ORDER_NAME, ORDER_PRICE, ORDER_QUANTITY = [], [], []
+        global ORDER_NAME, ORDER_PRICE, ORDER_QUANTITY, ORDER_STATUS
+        ORDER_NAME, ORDER_PRICE, ORDER_QUANTITY, ORDER_STATUS = [], [], [], []
         for posted_requests in REQUESTED_DATA: #REQUESTED_DATA will be a list of list
             for order in posted_requests:
                 CURRENT_ID+=1
@@ -34,11 +34,12 @@ def orders():
                 ORDER_QUANTITY.append(order['quantity'])
                 ORDER_DELIVERY_ADDRESS.append(order['address'])
                 ORDER_CONTACT.append(order['contact'])
+                ORDER_STATUS.append('New')
         CURRENT_ID = 0 #reset
                 
         return jsonify({'name':ORDER_NAME, 'id':ORDER_ID, 'price':ORDER_PRICE,
                         'quantity' : ORDER_QUANTITY, 'address': ORDER_DELIVERY_ADDRESS,
-                        'contact':ORDER_CONTACT})
+                        'contact':ORDER_CONTACT,'status':ORDER_STATUS})
     #else if request.method == 'GET'
     try:
         ALL_ORDERS = []
@@ -48,7 +49,8 @@ def orders():
                         'price':ORDER_PRICE[ids-1],
                         'quantity' : ORDER_QUANTITY[ids-1],
                         'address': ORDER_DELIVERY_ADDRESS[ids-1],
-                        'contact':ORDER_CONTACT[ids-1]}) 
+                        'contact':ORDER_CONTACT[ids-1],
+                        'status':ORDER_STATUS[ids-1]}) 
         return jsonify(ALL_ORDERS)
         
     except NameError:
@@ -67,25 +69,23 @@ def show_order(specific_order_id):
                                 'price':ORDER_PRICE[specific_order_id-1],
                                 'quantity' : ORDER_QUANTITY[specific_order_id-1],
                                 'address': ORDER_DELIVERY_ADDRESS[specific_order_id-1],
-                                'contact':ORDER_CONTACT[specific_order_id-1]})
+                                'contact':ORDER_CONTACT[specific_order_id-1],
+                                'status':ORDER_STATUS[specific_order_id-1]},)
         except NameError:
             return jsonify(NAME_ERROR)
         except IndexError:
             return jsonify(INDEX_ERROR)
-    #elif request.method == 'PUT':
+    #elif request.method == 'PUT':     #THIS UPDATES STATUS OF AN ORDER
     try:
         #request.json will always be one order for put request[ideally].If not so the first will always be picked 
-        ORDER_NAME[specific_order_id-1] = request.json[0]['name']
-        ORDER_PRICE[specific_order_id-1] = request.json[0]['price']
-        ORDER_QUANTITY[specific_order_id-1] = request.json[0]['quantity']
-        ORDER_DELIVERY_ADDRESS[specific_order_id-1] = request.json[0]['address']
-        ORDER_CONTACT[specific_order_id-1] = request.json[0]['contact']
+        ORDER_STATUS[specific_order_id-1] = request.json[0]['status']
         return jsonify({'name':ORDER_NAME[specific_order_id-1],
                         'id':ORDER_ID[specific_order_id-1],
                         'price':ORDER_PRICE[specific_order_id-1],
                         'quantity' : ORDER_QUANTITY[specific_order_id-1],
                         'address': ORDER_DELIVERY_ADDRESS[specific_order_id-1],
-                        'contact':ORDER_CONTACT[specific_order_id-1]})
+                        'contact':ORDER_CONTACT[specific_order_id-1],
+                        'status':ORDER_STATUS[specific_order_id-1]})
     except NameError:
         return jsonify(NAME_ERROR)
     except IndexError:
