bde2d0a39c560dd99f0e8b383523220e92833fb5
Ronny Mageh <ronnymageh@gmail.com>
2018-10-03 03:06:15 +0300

    [Feature #160770485] Added admin route for fetching specific orders

--- api/v2/orders.py
+++ api/v2/orders.py
@@ -21,7 +21,7 @@ class Orders:
                         order_contact,order_status,user_id, meal_name) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
         self.message = "Order placed successfully."
         self.error = "Unable to place order. The meal is not available"
-    def order_history(self):
+    def user_history(self):
         """Order_history helps the user see all his/her orders ever placed"""
         self.query_1 = "SELECT * FROM orders WHERE user_id=%s"
         self.input_1 = (self.user_id,) 
@@ -34,6 +34,17 @@ class Orders:
         self.message = "Successfully fetched all orders."
         self.error = "Unable to fetch all orders"
         self.event = "admin_get_all_orders"
+    
+    def get_specific_order(self,order_id):
+        """This function enables the admin to fetch a specific order. Input is order_id"""
+        self.query = "SELECT * FROM orders WHERE order_id=%s"
+        self.input = (order_id,)     #tuple to support indexing
+        self.query_1 = "SELECT order_id FROM orders ORDER BY order_id DESC LIMIT 1."
+        self.event = "admin_get_specific_order"
+        self.error = "Invalid order id"
+        self.message = "Successfully fetched the order."
+        self.order_id = order_id
+        self.db_error = None
           
     def connect_db(self):
         self.status = 0
@@ -62,7 +73,7 @@ class Orders:
                 self.history = []
                 for i in range(len(self.orders)):
                     self.history.append({'order_id':self.orders[i][0],
-                                'order_price':str(self.orders[i][1]),
+                                'order_price':str(self.orders[i][1]),    #Decimal type not JSON seriailzable
                                 'order_delivery_address':self.orders[i][2],
                                 'order_quantity':self.orders[i][3],
                                 'order_contact' :self.orders[i][4],
@@ -88,6 +99,27 @@ class Orders:
                                 'user_id':self.orders[i][6],
                                 'meal_name':self.orders[i][7]
                                 })
+                cur.close()
+                conn.close()
+            elif self.event == "admin_get_specific_order":
+                #first get number of orders
+                cur.execute(self.query_1)
+                number_of_orders = cur.fetchone()
+                if self.order_id in range(1,number_of_orders[0]+1):
+                    
+                    cur.execute(self.query, self.input)
+                    specific_order = cur.fetchone()
+                    self.specific_order = {'order_id':specific_order[0],
+                                    'order_price':str(specific_order[1]),    #Decimal type not JSON seriailzable
+                                    'order_delivery_address':specific_order[2],
+                                    'order_quantity':specific_order[3],
+                                    'order_contact' :specific_order[4],
+                                    'order_status' :specific_order[5],
+                                    'user_id' :specific_order[6],
+                                    'meal_name' :specific_order[7],
+                                    }
+                else: #Order_id does not exist
+                    self.status = 1
                 cur.close()
                 conn.close()
         except (Exception, psycopg2.DatabaseError) as error:
