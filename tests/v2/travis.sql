55113d85fb9a2c570f772989b806cb6eeecd63db
Ronny Mageh <ronnymageh@gmail.com>
2018-10-08 16:08:50 +0300

    [chore #160963333] travis build fix

--- tests/v2/travis.sql
+++ tests/v2/travis.sql
@@ -4,10 +4,10 @@ CREATE USER tester WITH PASSWORD 'travis-test';
 
 CREATE DATABASE test_fast_food_fast;
 \c test_fast_food_fast
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON DATABASE test_fast_food_fast TO tester;
+GRANT ALL PRIVILEGES ON DATABASE test_fast_food_fast TO tester;
 CREATE SCHEMA IF NOT EXISTS public;
         
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     email VARCHAR(255) PRIMARY KEY ,
     user_id VARCHAR(255) UNIQUE,
     user_name VARCHAR(255) NOT NULL,
@@ -15,13 +15,13 @@ CREATE TABLE IF NOT EXISTS users (
     user_type VARCHAR(15) NOT NULL,
     user_token VARCHAR(255)
 )
- CREATE TABLE IF NOT EXISTS menu (
+ CREATE TABLE menu (
             
                 meal_id SERIAL UNIQUE ,
                 meal_name VARCHAR(50) PRIMARY KEY ,
                 meal_price DECIMAL(6,2) NOT NULL
                 )
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE orders (
             
                 order_id SERIAL PRIMARY KEY,
                 order_price DECIMAL(6,2) NOT NULL,
